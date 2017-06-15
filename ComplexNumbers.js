9f4f31b5ad17e846c1df6aa8297f7a71e6db092b
maxkaliner <maxkaliner@yahoo.com>
2017-06-14 22:32:38 -0400

    Added panning, new color scheme, zoom slider, and tap while holding shift to change imaginary and real values

--- ComplexNumbers.js
+++ ComplexNumbers.js
@@ -1,4 +1,5 @@
 class ComplexNum {
+    
     constructor(real, imag) {
         this.real = real;
         this.imag = imag;
@@ -8,22 +9,89 @@ class ComplexNum {
         var newimag;
         var oldreal;
         var oldimag;
-        newreal = newNum.real;
-        newimag = newNum.imag;
+        if(!(document.getElementById("mandelbrot").checked)) {
+            newreal = newNum.real;
+            newimag = newNum.imag;
+        }
+        else {
+            newreal = 0;
+            newimag = 0;
+            reC = newNum.real;
+            imC = newNum.imag;
+        }
+        var smoothcolor = Math.exp(-Math.sqrt(Math.pow(newreal,2)+Math.pow(newimag,2)));
         
         var i;
-        for(i=0; i<maxiterations; i++) {
-            oldreal = newreal;
-            oldimag = newimag;
-            newreal = (oldreal*oldreal)+(-(oldimag*oldimag))-reC;
-            newimag = 2*(oldimag*oldreal)-imC;
-            if((newreal*newreal)+(newimag*newimag)>4) {
-                break;
+        
+        if(document.getElementById("quadeq").checked) {
+            for(i=0; i<maxiterations; i++) {
+                oldreal = newreal;
+                oldimag = newimag;
+                newreal = (oldreal*oldreal)+(-(oldimag*oldimag))-reC;
+                newimag = 2*(oldimag*oldreal)-imC;
+                smoothcolor += Math.exp(-Math.sqrt(Math.pow(newreal,2)+Math.pow(newimag,2)));
+                if((newreal*newreal)+(newimag*newimag)>4) {
+                    break;
+                }
+                
             }
-            
         }
-        fill = HSVtoRGB((i%361)/360, 100/100, (100*(i<maxiterations))/100);
-        ctx.fillStyle = "rgb("+fill.r+","+fill.g+","+fill.b+")";
+        if(document.getElementById("cubiceq").checked) {
+            for(i=0; i<maxiterations; i++) {
+                oldreal = newreal;
+                oldimag = newimag;
+                newreal = (oldreal*oldreal*oldreal)+(-(3*oldimag*oldimag*oldreal))-reC;
+                newimag = (3*oldimag*oldreal*oldreal)+(-(oldimag*oldimag*oldimag))-imC;
+                smoothcolor += Math.exp(-Math.sqrt(Math.pow(newreal,2)+Math.pow(newimag,2)));
+                if((newreal*newreal)+(newimag*newimag)>4) {
+                    break;
+                }
+                
+            }
+        }
+        if(document.getElementById("quarticeq").checked) {
+            for(i=0; i<maxiterations; i++) {
+                oldreal = newreal;
+                oldimag = newimag;
+                newreal = (oldreal*oldreal*oldreal*oldreal)+(-(6*oldimag*oldimag*oldreal*oldreal))+(oldimag*oldimag*oldimag*oldimag)-reC;
+                newimag = (4*oldimag*oldreal*oldreal*oldreal)+(-(4*oldimag*oldimag*oldimag*oldreal))-imC;
+                smoothcolor += Math.exp(-Math.sqrt(Math.pow(newreal,2)+Math.pow(newimag,2)));
+                if((newreal*newreal)+(newimag*newimag)>4) {
+                    break;
+                }
+                
+            }
+        }
+        if(document.getElementById("mandelbrot").checked) {
+            for(i=0; i<maxiterations; i++) {
+                oldreal = newreal;
+                oldimag = newimag;
+                newreal = (oldreal*oldreal)+(-(oldimag*oldimag))-reC;
+                newimag = 2*(oldimag*oldreal)-imC;
+                smoothcolor += Math.exp(-Math.sqrt(Math.pow(newreal,2)+Math.pow(newimag,2)));
+                if((newreal*newreal)+(newimag*newimag)>4) {
+                    break;
+                }
+                
+            }
+        }
+        
+        smoothcolor = (0.95+10*smoothcolor);
+        if(smoothcolor>360) {
+            smoothcolor = 360;
+        }
+        smoothcolor = smoothcolor/(3/2)+120*2
+        
+        
+        fill = HSVtoRGB(((smoothcolor))/360, 100/100, ((i<maxiterations)));
+        
+        
+        ctx.fillStyle = "rgb("+(fill.r)+","+(fill.g)+","+(fill.b)+")";
+        // ctx.fillStyle = "rgb("+(255-fill.r)%256+","+(255-fill.g)%256+","+(255-fill.b)%256+")";
+        // ctx.fillStyle = "rgb("+(255-fill.r)+","+(255-fill.g)+","+(255-fill.b)+")";
+//        ctx.fillStyle = "rgb("+(Math.sin(i/3))+","+(Math.cos(i/6))+","+(Math.cos(i/12+3.14/4))+")";
+
     }
+    
     
 }
--- drawJuliaSet.js
+++ drawJuliaSet.js
@@ -1,25 +1,112 @@
 function drawGraph() {
+    document.getElementById("realVal").defaultValue = 0.7;
+    
+    document.getElementById("imaginaryVal").defaultValue = 0.29;
+    
+    document.getElementById("zoom").defaultValue = 185;
+    
+    document.getElementById("maxiterations").defaultValue = 200;
+    
+    document.getElementById("xpos").defaultValue = 0;
+    
+    document.getElementById("ypos").defaultValue = 0;
+    
     c = document.getElementById("canvas");
     ctx = c.getContext("2d");
-    c.width = 640;
-    c.height = 640;
+    c.width = 400;
+    c.height = 400;
     xaxis = c.width;
     yaxis = c.height;
-    newNum = new ComplexNum(0,0);
+//    newNum = new ComplexNum(0,0);
     pixelIdentifier = 0;
     fill = 0;
-    document.getElementById("realVal").defaultValue = 0.7;
-    document.getElementById("imaginaryVal").defaultValue = 0.29;
     reC = document.getElementById("realVal").value;
     imC = document.getElementById("imaginaryVal").value;
-    document.getElementById("zoom").defaultValue = 185;
     zoom = document.getElementById("zoom").value;
-    document.getElementById("maxiterations").defaultValue = 200;
     maxiterations = document.getElementById("maxiterations").value;
-    document.getElementById("xpos").defaultValue = 0;
-    document.getElementById("ypos").defaultValue = 0;
     xpos = document.getElementById("xpos").value;
     ypos = document.getElementById("ypos").value;
+    
+    keypressed = false;
+    b = 0;
+    
+    
+    
+    
+    function download() {
+        var dt = canvas.toDataURL('image/jpeg');
+        this.href = dt;
+    };
+    downloadLnk.addEventListener('click', download, false);
+    
+    eventdown = function(evt) {
+        mouseposdown = getMousePos(canvas, evt);
+    }
+    
+    makeHighRes(c);
+    
+    event = function event(evt) {
+        mouseposup = getMousePos(canvas, evt);
+        if(keypressed) {
+            document.getElementById("realVal").value = (mouseposup.x-(xaxis/2))/(zoom);
+            document.getElementById("imaginaryVal").value =
+            -(mouseposdown.y-(yaxis/2))/(zoom);
+        }
+        else if(mouseposup.x!=mouseposdown.x && mouseposup.y!=mouseposdown.y){
+            document.getElementById("xpos").value -=
+            (-(((mouseposup.x-mouseposdown.x)/zoom)));
+            document.getElementById("ypos").value -=
+            (-(((mouseposdown.y-mouseposup.y)/zoom)));
+        }
+        else {
+            document.getElementById("xpos").value -= (mouseposup.x-(xaxis/2))/(zoom);
+            document.getElementById("ypos").value -=
+            -(mouseposdown.y-(yaxis/2))/(zoom);
+        }
+        
+        document.getElementsByTagName("IMG")[0].removeAttribute("hidden");
+        
+        drawGraph();
+    }
+    
+    function keypresstrue(evt) {
+        // if(evt.keyCode==16) {
+        keypressed = true;
+        // }
+        
+    }
+    function keypressfalse(evt) {
+        // if(evt.keyCode==16) {
+        keypressed = false;
+        // }
+    }
+    
+    
+    
+    if(typeof q == 'undefined') {
+        q=1;
+        canvas.addEventListener('mousedown', eventdown, false);
+        canvas.addEventListener('mouseup', event, false);
+        canvas.addEventListener('keydown', keypresstrue, false);
+        canvas.addEventListener('keyup', keypressfalse, false);
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+        x: evt.clientX-rect.left,
+        y: evt.clientY-rect.top-0.125
+        };
+    }
+    
     
     
     for(var y=0; y<yaxis; y++) {
@@ -33,21 +120,54 @@ function drawGraph() {
     ctx.beginPath();
     lineWidth = 1;
     
-    ctx.moveTo(Math.round(xaxis/2)+0.5+(zoom*xpos), 0.5-2*(zoom*ypos));
-    ctx.lineTo(Math.round(xaxis/2)+0.5+(zoom*xpos), yaxis+0.5+2*(zoom*ypos));
+    ctx.moveTo(Math.round(xaxis/2)+0.5+(zoom*xpos), 0.5-2*(zoom));
+    ctx.lineTo(Math.round(xaxis/2)+0.5+(zoom*xpos), yaxis+0.5+2*(zoom));
     
-    ctx.moveTo(0.5-2*(zoom*xpos),Math.round(yaxis/2)+0.5-(zoom*ypos));
-    ctx.lineTo(xaxis+0.5+2*(zoom*xpos), Math.round(yaxis/2)+0.5-(zoom*ypos));
-    
-    
+    ctx.moveTo(0.5-2*(zoom),Math.round(yaxis/2)+0.5-(zoom*ypos));
+    ctx.lineTo(xaxis+0.5+2*(zoom), Math.round(yaxis/2)+0.5-(zoom*ypos));
     ctx.stroke();
+    
+    
+    
+    
     
     return false;
 }
 
 function drawJuliaSet(x, y) {
-    newNum = new ComplexNum((x-(xaxis/2))/zoom-xpos, ((y-(yaxis/2))/zoom)+1*ypos);
+    newNum = new ComplexNum((x-(xaxis/2))/zoom-xpos,
+                            ((y-(yaxis/2))/zoom)+1*ypos);
+    
     newNum.testinf(pixelIdentifier);
     ctx.fillRect(x,y,1,1);
 }
 
+function zoomsliderfunc() {
+    document.getElementById("zoom").value = Math.pow(2,(document.getElementById("zoomslider").value)/500);
+    drawGraph();
+}
+
+function makeHighRes(c) {
+    // finally query the various pixel ratios
+    var devicePixelRatio = window.devicePixelRatio || 1;
+    var backingStoreRatio = ctx.webkitBackingStorePixelRatio ||
+    ctx.mozBackingStorePixelRatio ||
+    ctx.msBackingStorePixelRatio ||
+    ctx.oBackingStorePixelRatio ||
+    ctx.backingStorePixelRatio || 1;
+    var ratio = devicePixelRatio / backingStoreRatio;
+    // upscale canvas if the two ratios don't match
+    if (devicePixelRatio !== backingStoreRatio) {
+        
+        var oldWidth = c.width;
+        var oldHeight = c.height;
+        c.width = Math.round(oldWidth * ratio);
+        c.height = Math.round(oldHeight * ratio);
+        c.style.width = oldWidth + 'px';
+        c.style.height = oldHeight + 'px';
+        // now scale the context to counter
+        // the fact that we've manually scaled
+        // our canvas element
+        ctx.scale(ratio, ratio);
+    }
+}
